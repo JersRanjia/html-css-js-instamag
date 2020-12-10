f8bfbefbf3355b55e23fd15c0370cd9f32a7fa40
JersRanjia <jersrandria@yahoo.fr>
2020-12-10 21:20:50 +0300

    create menu-animation.js and link it to html

--- js/utils.js
+++ js/utils.js
@@ -6,6 +6,8 @@ var img_next = view_next.getElementsByTagName("img")[0];
 var img_main = view_main.getElementsByTagName("img")[0];
 var img_prev = view_prev.getElementsByTagName("img")[0];
 
+var bt_menu = document.getElementById("bt-menu")
+
 var list_firstImg = document.querySelectorAll(".mini-content img")
 
 var selected = 1 //2eme
